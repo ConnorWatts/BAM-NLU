723d1fe8777b9cee3ddb65fe14d9e25f48fc184e
Connor Watts <connorwatts@hotmail.com>
2022-12-07 14:16:57 +0000

    UpdaTE

---
+++ configue.py
@@ -0,0 +1,11 @@
+
+
+class Config():
+
+    '''
+    Class for handling hyperparameters for the experiment
+    
+    '''
+
+    def __init__(self, model_name, topdir, **kwargs):
+        pass
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from configue import Config
+
+import json
+
 
 def main():
 
     topdir = "C:/Users/cwatts/BAM NLU/BAM-NLU"
     model_name = "BAM-NLU"
-    hparams = {}
-    
+    hparams = '{}'
+
+    config = Config(topdir,model_name,**json.loads(hparams))
+
     pass
 
 if __name__ == "__main__":
