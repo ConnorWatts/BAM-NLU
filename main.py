92dcf76f9d0fff29569fcfee9244463a49e25ed2
Connor Watts <connorwatts@hotmail.com>
2022-12-08 11:47:35 +0000

    Delete config

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from configue import Config
 
-import json
-
+import argparse
 
 def main():
-
-    topdir = "C:/Users/cwatts/BAM NLU/BAM-NLU"
-    model_name = "BAM-NLU"
-    hparams = '{}'
-
-    config = Config(topdir,model_name,**json.loads(hparams))
-
     pass
 
-if __name__ == "__main__":
-    main()
+def get_args():
+
+    parser = argparse.ArgumentParser(description='DQN Pairs Trading')
+
+    parser.add_argument("--topdir", type=str, help="Top directory for project", default="C:/Users/cwatts/BAM NLU/BAM-NLU")
+    parser.add_argument("--model_name", type=str, help="Name of the model", default="BAM-NLU")
+
+    args = parser.parse_args()
+
+    # Wrapping training configuration into a dictionary
+    training_config = dict()
+    for arg in vars(args):
+        training_config[arg] = getattr(args, arg)
+
+    return training_config
+
+
+
+
+if __name__ == '__main__':
+    main(get_args())
